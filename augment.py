211bf3bc07d9f605cc439eb7e473eed32e1b6de1
mhoibo <maren.hoibo@ntnu.no>
2022-06-16 16:23:11 +0200

    added hue, saturation and shift

--- augment.py
+++ augment.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
 
 # Augmentations
-def random_brightness(x):  # look at second input parameter to random_brightness. How to deal with normalized (?)
+def random_brightness(x, brightness=25):  # look at second input parameter to random_brightness. How to deal with normalized (?)
     nbr = tf.random.uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
-    x = tf.cond(nbr < 0.5, lambda: x, lambda: tf.image.random_brightness(x, 0.1))
+    x = tf.cond(nbr < 0.5, lambda: x, lambda: tf.clip_by_value(tf.image.random_brightness(x, brightness), 0, 255))
     return x
 
 def random_fliplr(x, y):
@@ -18,8 +19,40 @@ def random_flipud(x, y):
 
 def random_rot90(x ,y):  # not 90 degrees when k = 1, why??????
     nbr = tf.random.uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
-    print("nbr", nbr)
-    nbr_rot = tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32)
-    print("nbr rot", nbr_rot)
+    nbr_rot = tf.random.uniform(shape=[], minval=1, maxval=4, dtype=tf.int32)
     x, y = tf.cond(nbr < 0.5, lambda: (x, y), lambda: (tf.image.rot90(x, k=nbr_rot), tf.image.rot90(y, k=nbr_rot)))
     return x, y
+
+def random_hue(x, max_delta):
+    nbr = tf.random.uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
+    x = tf.cond(nbr < 0.5, lambda: x, lambda: tf.clip_by_value(tf.image.random_hue(x, max_delta=max_delta), 0, 255))
+    return x
+
+def random_saturation(x, saturation):
+    nbr = tf.random.uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
+    x = tf.cond(nbr < 0.5, lambda: x, lambda: tf.clip_by_value(tf.image.random_saturation(x, lower=1-saturation, upper=1+saturation), 0, 255))
+    return x
+
+# convenience
+def gt_shift(z, shift_x, shift_y):
+    # need to handle one-hots in a specific way, when shifting
+    tmp1 = tfa.image.translate(z[..., 0:1], (shift_x, shift_y), fill_value=1.0)  # background special case!!
+    tmp2 = tfa.image.translate(z[..., 1:], (shift_x, shift_y), fill_value=0.0)  # all other classes, do same padding
+    z = tf.concat([tmp1, tmp2], axis=-1)
+    return z
+
+def random_shift(x, y, translate=50):
+    choice = tf.random.uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
+    shift_x = tf.random.uniform(shape=[], minval=-translate, maxval=translate, dtype=tf.int32)
+    shift_y = tf.random.uniform(shape=[], minval=-translate, maxval=translate, dtype=tf.int32)
+
+    x, y = tf.cond(choice < 0.5, lambda: (x, y), lambda: (
+        tfa.image.translate(x, (shift_x, shift_y)),
+        gt_shift(y, shift_x, shift_y),
+        )
+    )
+    return x, y
+
+# zoom
+ # think about interpolation
+ # think about gt is one-hot encoded
