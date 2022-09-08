ea0bca359ca547c9dbba574f4c9101a306ea56a3
maren <maren.hoibo@ntnu.no>
2022-09-08 11:46:06 +0200

    changed to batch level

--- eval.py
+++ eval.py
@@ -3,67 +3,82 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.models import load_model
 import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-ds_name = 'dataset_130622_143222_unet' # change manually to determine desired dataset
-model_name = 'model_130622_143222_unet' # change manually to determine desired model
-#bs = 4  # change manually to match batch size in train.py
-
-ds_test_path = './output/datasets/' + ds_name + '/ds_test'
-model_path = './output/models/' + model_name
-
-ds_test = tf.data.experimental.load(
-    ds_test_path, element_spec=None, compression=None, reader_func=None
-)
-model = load_model(model_path, compile=False)  # do not compile now, already done
-
-titles = ["image", "heatmap", "pred", "gt"]
+from tqdm import tqdm
+from utils import normalize_img, patchReader
+from stats import BCa_interval_macro_metric
 
 # make dice function (pred, gt) -> DSC value (float)
-def dice_loss(pred, gt, epsilon=1e-10):
+def dice_metric(pred, gt):
     smooth = 1.
-    dice = 0
     intersection1 = tf.reduce_sum(pred * gt)
     union1 = tf.reduce_sum(pred * pred) + tf.reduce_sum(gt * gt)
-    dice += (2. * intersection1 + smooth) / (union1 + smooth)
+    dice = (2. * intersection1 + smooth) / (union1 + smooth)
+    return dice
 
-    dice2 = 1 - dice
-    return tf.clip_by_value(1. - dice, 0., 1. - epsilon), dice2
+def eval_on_dataset():
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    plot_flag = False
+    bs = 32
 
-def pred():
+    ds_name = 'dataset_070922_154010_unet'  # change manually to determine desired dataset
+    model_name = 'model_070922_154010_unet'  # change manually to determine desired model
+    # bs = 4  # change manually to match batch size in train.py
+
+    ds_test_path = './output/datasets/' + ds_name + '/ds_train'
+    model_path = './output/models/' + model_name
+
+    # load generated tf test dataset
+    ds_test = tf.data.experimental.load(ds_test_path, element_spec=None, compression=None, reader_func=None)
+    ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.batch(bs)
+    ds_test = ds_test.prefetch(1)
+
+    # load model
+    model = load_model(model_path, compile=False)  # do not compile now, already done
+
+    # for plotting
+    titles = ["image", "heatmap", "pred", "gt"]
+
     dice_losses = []
     dis = []
     cnt = 0
-    for image, mask in ds_test:
+    for image, mask in tqdm(ds_test):
         #print(image.shape)
         #print(mask.shape)
-        pred_mask = model.predict(np.expand_dims(image, axis = 0))
+        # if image.shape[0] == 1:
+
+        pred_mask = model.predict_on_batch(image)
         threshold = (pred_mask >= 0.5).astype("float32")
         #pred_masks.append(pred_mask)
-        f, axes = plt.subplots(1, 4)  # Figure of the two corresponding TMAs
-        print("image shape", image.shape)
-        print("mask shape", mask.shape)
-        print("pred mask shape", pred_mask.shape)
-        axes[0].imshow(image)
-        axes[1].imshow(np.array(pred_mask[0, ..., 1]), cmap="gray")
-        axes[2].imshow(np.array(threshold[0, ..., 1]),cmap="gray")
-        axes[3].imshow(np.array(mask[..., 1]), cmap="gray")
-        [axes[i].set_title(title_) for i, title_ in enumerate(titles)]
-        plt.show()
+        if plot_flag:
+            f, axes = plt.subplots(1, 4)  # Figure of the two corresponding TMAs
+            print("image shape", image.shape)
+            print("mask shape", mask.shape)
+            print("pred mask shape", pred_mask.shape)
+            axes[0].imshow(image)
+            axes[1].imshow(np.array(pred_mask[0, ..., 1]), cmap="gray")
+            axes[2].imshow(np.array(threshold[0, ..., 1]),cmap="gray")
+            axes[3].imshow(np.array(mask[..., 1]), cmap="gray")
+            [axes[i].set_title(title_) for i, title_ in enumerate(titles)]
+            plt.show()
 
-        loss,di = dice_loss(threshold, mask)
-        dice_losses.append(loss)
-        dis.append(di)
-        print(loss)
-        print(di)
-        print('-----------')
+        dice = [dice_metric(threshold[i, ..., 1], mask[i, ..., 1]).numpy() for i in range(mask.shape[0])]
+        dice_losses.extend(dice)
+        #print(dice)
+        #print('-----------')
         #exit()
         cnt = cnt + 1
-        if cnt == 5:
-            exit()
-    mean = np.mean(dice_losses)
-    print(mean)
-    m_di = np.mean(dis)
-    print(m_di)
-pred()
+        #if cnt == 10:
+        #    break
+    mu_ = np.mean(dice_losses)
+    # print(mu_)
+    # print(np.std(dice_losses))
+
+    # create 95%-CI
+    dice_ci, _ = BCa_interval_macro_metric(dice_losses, func=lambda x: np.mean(x), B=10000)
+
+    print(mu_, dice_ci)
+
+
+if __name__ == "__main__":
+    eval_on_dataset()
