f15182fbe8554b63c5432cd45fe03b570322fc90
maren <maren.hoibo@ntnu.no>
2022-11-22 14:21:09 +0100

    Convert geoJSON to tif. Works for one image at a time.

--- convert_to_tiff.py
+++ convert_to_tiff.py
@@ -2,10 +2,13 @@
 Script to convert annotations to geoJSON, then tif, then pyramidal tiff
 Get shape of CK image
 """
+import json
 import os
 import fast
 import subprocess as sp
 from tqdm import tqdm
+import shutil
+import numpy as np
 
 fast.Reporter.setGlobalReportMethod(fast.Reporter.COUT)
 
@@ -14,6 +17,14 @@ def get_size(image_path, level):
     importerCK = fast.WholeSlideImageImporter.create(image_path)
     image = importerCK.runAndGetOutputData()
 
+    # get all metadata
+    print(image.getMetadata())
+    print(np.asarray(image.getMetadata()))
+    print(dict(image.getMetadata()))
+
+    # exit()
+
+    # get WSI dimensions
     height = image.getLevelHeight(level)
     width = image.getLevelWidth(level)
 
@@ -26,26 +37,33 @@ def get_size(image_path, level):
 level = 0  # image pyramid level to get size from
 
 CK_paths = "/data/Maren_P1/epithelium/CK/"
-geojson_path = "/home/maren/workspace/qupath-ck-seg/export_geojson_201022/"
-output_path = "/home/maren/workspace/qupath-ck-seg/pyramidal_tiff/"
+geojson_path = "/home/maren/workspace/qupath-ck-seg/export_geojson_031122b/"
 tmp_path = "/home/maren/workspace/qupath-ck-seg/tmp_results/"
+output_path = "/home/maren/workspace/qupath-ck-seg/pyramidal_tiff/"
+
+# delete folders
+shutil.rmtree(tmp_path)
+shutil.rmtree(output_path)
+
+os.makedirs(tmp_path, exist_ok=True)
+os.makedirs(output_path, exist_ok=True)
 
 wsis = []
 for file in os.listdir(CK_paths):
     if (".vsi" in file) and ("Overview" not in file):
         wsis.append(CK_paths + file)
 
-wsis = wsis[::-1]
+# wsis = wsis[:-1]
 
 for pathCK in tqdm(wsis, "WSI:"):
+    print(pathCK)
+    if not pathCK.endswith("4.vsi"):
+        continue
     # get ID
     id_ = pathCK.split("/")[-1].split(".")[0]
 
-    print(pathCK)
-
-    # get image metadata info
-    height, width, spacing = get_size(pathCK, level=level)
-    print(height, width, spacing)
+    height =  # height from qupath/bioformats
+    width = # width from qupath/bioformats
 
     # subprocesses to run terminal commands from python script
     file1 = geojson_path + id_ + ".vsi - EFI 40x-labels.geojson"  # path to geoJSON file
@@ -53,10 +71,9 @@ for pathCK in tqdm(wsis, "WSI:"):
     file3 = output_path + id_ + ".tiff"  # path to tiff file (pyramid)
 
     # convert from geoJSON to tiled geoTIFF (TIFF)
-    sp.check_call(["gdal_rasterize", "-burn", "1", "-ts", str(width), str(height), "-ot",
+    sp.check_call(["gdal_rasterize", "-burn", "1", "-ts", str(width), str(height), "-te", "0", "0", str(width), str(height), "-ot",
                    "Byte", file1, file2, "-co", "COMPRESS=LZW"])
 
     # convert from TIFF to pyramidal TIFF
     sp.check_call(["vips", "tiffsave", file2, file3, "--bigtiff", "--tile", "--pyramid",
                    "--compression=lzw"])  # do I need to change quality of compression? "--Q=" + str(Q2)?
-
