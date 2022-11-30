da0d884613578ec3fb32aee410a9cd2b1f4ffdcd
maren <maren.hoibo@ntnu.no>
2022-11-30 09:57:23 +0100

    Javabridge to get size

--- convert_to_tiff.py
+++ convert_to_tiff.py
@@ -1,49 +1,34 @@
 """
-Script to convert annotations to geoJSON, then tif, then pyramidal tiff
+Script to convert annotations from geoJSON to geoTIFF, then pyramidal TIFF
 Get shape of CK image
 """
-import json
 import os
 import fast
 import subprocess as sp
 from tqdm import tqdm
 import shutil
-import numpy as np
+import javabridge as jb
+import bioformats as bf
 
 fast.Reporter.setGlobalReportMethod(fast.Reporter.COUT)
 
+# start java VM
+jb.start_vm(class_path=bf.JARS)
 
-def get_size(image_path, level):
-    importerCK = fast.WholeSlideImageImporter.create(image_path)
-    image = importerCK.runAndGetOutputData()
-
-    # get all metadata
-    print(image.getMetadata())
-    print(np.asarray(image.getMetadata()))
-    print(dict(image.getMetadata()))
-
-    # exit()
-
-    # get WSI dimensions
-    height = image.getLevelHeight(level)
-    width = image.getLevelWidth(level)
-
-    # spacing = image.getSpacing()
-    spacing = None  # @TODO: Spacing [1, 1, 1] not found in FAST using cellSens VSI format
-    return height, width, spacing
-
+# mute BF
+log_config = os.path.join(os.path.split(__file__)[0], "resources", "log4j.properties")
 
 # Parameters:
 level = 0  # image pyramid level to get size from
 
 CK_paths = "/data/Maren_P1/epithelium/CK/"
-geojson_path = "/home/maren/workspace/qupath-ck-seg/export_geojson_031122b/"
+geojson_path = "/home/maren/workspace/qupath-ck-seg/export_geojson_291122/"
 tmp_path = "/home/maren/workspace/qupath-ck-seg/tmp_results/"
 output_path = "/home/maren/workspace/qupath-ck-seg/pyramidal_tiff/"
 
 # delete folders
 shutil.rmtree(tmp_path)
-shutil.rmtree(output_path)
+#shutil.rmtree(output_path)  # usually keep this, but had one in there already that I wanted to keep
 
 os.makedirs(tmp_path, exist_ok=True)
 os.makedirs(output_path, exist_ok=True)
@@ -57,13 +42,21 @@ for file in os.listdir(CK_paths):
 
 for pathCK in tqdm(wsis, "WSI:"):
     print(pathCK)
-    if not pathCK.endswith("4.vsi"):
+    if not pathCK.endswith("3.vsi"):
         continue
+
     # get ID
     id_ = pathCK.split("/")[-1].split(".")[0]
 
-    height =  # height from qupath/bioformats
-    width = # width from qupath/bioformats
+    ImageReader = bf.formatreader.make_image_reader_class()
+
+    # with ImageReader() as reader:
+    reader = ImageReader()
+    reader.setId(pathCK)
+    reader.setSeries(0)
+    height = reader.getSizeY()
+    width = reader.getSizeX()
+    reader.close()
 
     # subprocesses to run terminal commands from python script
     file1 = geojson_path + id_ + ".vsi - EFI 40x-labels.geojson"  # path to geoJSON file
@@ -71,9 +64,13 @@ for pathCK in tqdm(wsis, "WSI:"):
     file3 = output_path + id_ + ".tiff"  # path to tiff file (pyramid)
 
     # convert from geoJSON to tiled geoTIFF (TIFF)
-    sp.check_call(["gdal_rasterize", "-burn", "1", "-ts", str(width), str(height), "-te", "0", "0", str(width), str(height), "-ot",
-                   "Byte", file1, file2, "-co", "COMPRESS=LZW"])
+    sp.check_call(
+        ["gdal_rasterize", "-burn", "1", "-ts", str(width), str(height), "-te", "0", "0", str(width), str(height),
+         "-ot", "Byte", file1, file2, "-co", "COMPRESS=LZW"])
 
     # convert from TIFF to pyramidal TIFF
     sp.check_call(["vips", "tiffsave", file2, file3, "--bigtiff", "--tile", "--pyramid",
                    "--compression=lzw"])  # do I need to change quality of compression? "--Q=" + str(Q2)?
+
+# kill java VM
+jb.kill_vm()
