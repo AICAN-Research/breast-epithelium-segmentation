7d90c4a235461cfcc0f0f7b8c46732f21961b841
maren <maren.hoibo@ntnu.no>
2022-12-09 13:57:16 +0100

    cleaned code

--- train_plot.py
+++ train_plot.py
@@ -3,10 +3,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+epochs = 360  # match number of epochs in train script
 
-epochs = 100  # match number of epochs in train script
-
-history_path = './output/history/history_080922_155444_unet_bs_16.csv'  # path to history file
+history_path = './output/history/history_091222_090900_unet_bs_16.csv'  # path to history file
 
 data = pd.read_csv(history_path)
 
@@ -16,8 +15,8 @@ loss = data['loss']
 val_loss = data['val_loss']
 
 epochs = range(1, epochs + 1)
-plt.plot(epochs, loss, '-', label = 'Training loss')
-plt.plot(epochs, val_loss, '-', label = 'Validation loss')
+plt.plot(epochs, loss, '-', label='Training loss')
+plt.plot(epochs, val_loss, '-', label='Validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
