70b45009a535d58b7617fb97aaf1c3f74e37da09
maren <maren.hoibo@ntnu.no>
2023-01-25 15:11:17 +0100

    new split ratio, save as hdf5 instead of csv

--- divide_data.py
+++ divide_data.py
@@ -1,10 +1,11 @@
 # Divide data into train/validation and test set. The test set should not be used during development of the model.
 # This script should only be run to divide once.
+# Use fraction of wsis to begin with, then these have to be correctly placed when using all
 # Wait with cross-validation to later
 from datetime import datetime, date
 import os
 import numpy as np
-import pandas as pd
+import h5py
 
 
 mask_path = '/data/Maren_P1/data/annotations_converted/blue_channel_tiff/'
@@ -13,32 +14,27 @@ curr_date = "".join(date.today().strftime("%d/%m").split("/")) + date.today().st
 curr_time = "".join(str(datetime.now()).split(" ")[1].split(".")[0].split(":"))
 data_split_path = './data_splits/' + curr_date + "_" + curr_time + "/"
 
-files = os.listdir(mask_path)
+
+files = np.array(os.listdir(mask_path))
 nbr_files = len(files)
 
-N = nbr_files
-N_train = int(N * 0.75)
-N_val = int(np.floor(N * 0.15))
-N_test = int(N - N_train - N_val)
+N = nbr_files  # remaining files after manually put some in train set
+N_train = 16
+N_val = 4
 
 # shuffle
-order = np.arange(nbr_files)
-np.random.shuffle(order)
-shuffled_files = [files[x] for x in order]
+np.random.shuffle(files)
 
 # files in train, validation and test set
-train_set = shuffled_files[:N_train]
-val_set = shuffled_files[N_train:(N_train + N_val)]
-test_set = shuffled_files[(N_train + N_val):]
+train_set = files[:N_train]
+val_set = files[N_train:(N_train + N_val)]
+test_set = files[(N_train + N_val):]
 
-# convert to DataFrame
-labels = {'train': [train_set], 'val': [val_set], 'test': [test_set]}
-df = pd.DataFrame(data=labels, columns=['train', 'val', 'test'])
-
-# create folder if not exists
+# make folder if not exist
 os.makedirs(data_split_path, exist_ok=True)
 
-# save df as csv:
-df.to_csv(data_split_path + 'data_split.csv')
-
-
+# convert to hdf5
+with h5py.File((data_split_path + "dataset_split.h5"), 'w') as f:
+    f.create_dataset("test", data=np.array(test_set).astype('S400'), compression="gzip", compression_opts=4)
+    f.create_dataset("val", data=np.array(val_set).astype('S400'), compression="gzip", compression_opts=4)
+    f.create_dataset("train", data=np.array(train_set).astype('S400'), compression="gzip", compression_opts=4)
