accde722475063ae85a2625845b5c58adcf8dab6
maren <maren.hoibo@ntnu.no>
2023-02-02 16:58:49 +0100

    separate dice loss for multiple classes added

--- source/utils.py
+++ source/utils.py
@@ -6,43 +6,23 @@ import tensorflow_datasets as tfds
 import h5py
 
 
-def dice_metric(pred, gt):
-    smooth = 1.
-    intersection1 = tf.reduce_sum(pred * gt)
-    union1 = tf.reduce_sum(pred * pred) + tf.reduce_sum(gt * gt)
-    dice = (2. * intersection1 + smooth) / (union1 + smooth)
-    return dice
+def class_dice_loss(class_val, metric_name):
+    def dice_loss(y_true, y_pred):
+        smooth = 1
+        output1 = y_pred[:, :, :, class_val]
+        gt1 = y_true[:, :, :, class_val]
 
+        intersection1 = tf.reduce_sum(output1 * gt1)
+        union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(gt1 * gt1)
+        dice = (2. * intersection1 + smooth) / (union1 + smooth)
 
-# Used guide for custom_callbacks (https://www.tensorflow.org/guide/keras/custom_callback) and
-# custom metrics (https://www.tensorflow.org/guide/keras/train_and_evaluate) from
-# tensorflow keras
-class LossMetrics():
-    """
-    Create state variables for metric
-    """
-    def __init__(self, name="train_invasive_loss"):
-        self.train_invasive_loss =
-        self.train_benign_loss =
-        self.train_insitu_loss =
-        self.val_invasive_loss =
-        self.val_benign_loss =
-        self.val_insitu_loss =
+        # self.dice_values.assign_add((1 - dice) / 10)
+        return 1 - dice
 
-    def update_state(self, y_true, y_pred):
-    """
-    Update state variables using model predictions (y_pred) and target (y_true)
-    """
-    def result(self):
-    """
-    Compute final results
-    """
+    # set name of metric to be shown in tensorflow progress bar
+    dice_loss.__name__ = metric_name
 
-    def reset_state(self):
-    """
-    Reinitializes state of metric
-    """
-
+    return dice_loss
 
 
 # from tensorflow example, modified
