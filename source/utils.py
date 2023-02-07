0fc8dfd898001b60cf3ee6af2d64ed4de3c98e90
maren <maren.hoibo@ntnu.no>
2023-02-07 10:54:12 +0100

    added focal dice loss

--- source/utils.py
+++ source/utils.py
@@ -6,6 +6,63 @@ import tensorflow_datasets as tfds
 import h5py
 
 
+# focal dice loss to focus on "difficult classes"
+# implemented by André Pedersen:
+def categorical_focal_dice_loss(nb_classes, use_background=False, gamma=2):
+    def focal_dice_loss(target, output, epsilon=1e-10):
+        smooth = 1
+
+        # calculate Dice for each class separately and accumulate
+        dice = 0
+        for class_val in range(0 if use_background else 1, nb_classes):
+            # get prediction and target for current class only
+            output1 = output[
+                ..., class_val]  # @TODO: doing "..." is probably slower than doing [:, :, :, class_val], but works N-D
+            target1 = target[..., class_val]
+
+            # calculate Dice for current class
+            intersection1 = tf.reduce_sum(output1 * target1)
+            union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(target1 * target1)
+            dice += (2. * intersection1 + smooth) / (union1 + smooth)
+
+        # divide accumulated dice by class count -> macro-average Dice
+        dice /= (nb_classes - 1)
+
+        # perform negative of Dice and focal transform to get Focal-Dice loss
+        focal_dice = tf.math.pow((1. - dice), gamma)
+        return tf.clip_by_value(1. - focal_dice, 0., 1. - epsilon)
+
+    return focal_dice_loss
+
+
+def class_categorical_focal_dice_loss(class_val, metric_name, gamma=2):
+    def focal_dice_loss(target, output, epsilon=1e-10):
+        smooth = 1
+
+        # calculate Dice for each class separately and accumulate
+        dice = 0
+        # get prediction and target for current class only
+        output1 = output[:, :, :, class_val]
+        target1 = target[:, :, :, class_val]
+
+        # calculate Dice for current class
+        intersection1 = tf.reduce_sum(output1 * target1)
+        union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(target1 * target1)
+        dice += (2. * intersection1 + smooth) / (union1 + smooth)
+
+        # divide accumulated dice by class count -> macro-average Dice
+        #dice /= (nb_classes - 1)
+
+        # perform negative of Dice and focal transform to get Focal-Dice loss
+        focal_dice = tf.math.pow((1. - dice), gamma)
+        return tf.clip_by_value(1. - focal_dice, 0., 1. - epsilon)
+
+        # set name of metric to be shown in tensorflow progress bar
+
+    focal_dice_loss.__name__ = metric_name
+
+    return focal_dice_loss
+
 def class_dice_loss(class_val, metric_name):
     def dice_loss(y_true, y_pred):
         smooth = 1
