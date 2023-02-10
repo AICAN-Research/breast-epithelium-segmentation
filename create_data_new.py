bb2855d4d2b9b6ad9dfeb47e02abdee66d03e9fa
maren <maren.hoibo@ntnu.no>
2023-02-10 16:16:09 +0100

    cleaned up

--- create_data_new.py
+++ create_data_new.py
@@ -249,7 +249,7 @@ def create_datasets(he_path, ck_path, mask_path, annot_path, remove_path, datase
                 position_he_x /= (2 ** level)
                 position_he_y /= (2 ** level)
 
-                # @TODO: should heigh_mask be longest_height?
+                # @TODO: should height_mask be longest_height?
                 position_ck_y = height_mask - position_ck_y - height_ck
 
                 # get corresponding TMA core in the dab image as in the CK:
