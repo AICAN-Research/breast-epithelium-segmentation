3056904e6825a59b881d223871161bf87bcb0599
maren <maren.hoibo@ntnu.no>
2023-03-01 14:26:02 +0100

    height, width of wsis

--- create_data_wsi.py
+++ create_data_wsi.py
@@ -1,12 +1,16 @@
 """
 Script to create patches from whole slide images
+Annotations from QuPath of which areas to avoid
+Put patches in train/val/test set
+Which wsis into which set is determined previously
 """
 from datetime import datetime, date
 import h5py
 import numpy as np
 import fast
 
-def create_dataset(he_path, ck_path, annot_path):
+
+def create_dataset(he_path, ck_path, annot_path, dataset_path, level, patch_size, ds_factor):
     importer_he = fast.WholeSlideImageImporter.create(
         he_path)  # path to CK image
     importer_ck = fast.WholeSlideImageImporter.create(
@@ -14,15 +18,31 @@ def create_dataset(he_path, ck_path, annot_path):
     importer_annot = fast.TIFFImagePyramidImporter.create(
         annot_path)  # path to annotated image or areas to avoid
 
-    annot = importer_annot.runAndGetOutputData()
+    extractor_he = fast.ImagePyramidLevelExtractor.create(level=level).connect(importer_he)
+    extractor_ck = fast.ImagePyramidLevelExtractor.create(level=level).connect(importer_ck)
 
+    he_image = extractor_he.runAndGetOutputData()
+    ck_image = extractor_ck.runAndGetOutputData()
 
+    he_image = np.asarray(he_image)
+    ck_image = np.asarray(ck_image)
+
+    # get shape of he and ck images
+    he_height, he_width, _ = he_image.shape
+    ck_height, ck_width, _ = ck_image.shape
+
+    longest_height = max([he_height, ck_height])
+    longest_width = max([he_width, ck_width])
+
+    # create patches
+
+    # if patch includes areas in annotated image -> skip
 
 
 if __name__ == "__main__":
     level = 2
     patch_size = 1024
-    downsample_factor = 4
+    ds_factor = 4
 
     data_split_path = ""  # split train/val/test
     he_ck_path = ""  # path to he and ck slides
@@ -33,7 +53,7 @@ if __name__ == "__main__":
     dataset_path = "./datasets/" + curr_date + "_" + curr_time + \
                    "_level_" + str(level) + \
                    "_psize_" + str(patch_size) + \
-                   "_ds_" + str(downsample_factor) + "/"
+                   "_ds_" + str(ds_factor) + "/"
 
     # define datasets (train/val/test) - always uses predefined dataset
     with h5py.File(data_split_path, "r") as f:
