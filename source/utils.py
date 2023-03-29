f949fb3024a063dd01a40c40c77566f086cef951
maren <maren.hoibo@ntnu.no>
2023-03-29 13:33:30 +0200

    align images, detect and compute separately, cleaned up

--- source/utils.py
+++ source/utils.py
@@ -174,30 +174,28 @@ def alignImagesNew(im1, im2):
 
 
   # from: https://learnopencv.com/image-alignment-feature-based-using-opencv-c-python/
+  # and https://docs.opencv.org/3.4/d1/d89/tutorial_py_orb.html
 def alignImages(im1, im2, max_features=1000, good_match_percent=0.05):
     # Convert images to grayscale
-    #im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-    #im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
-    im1Gray = im1[..., 0]
-    im2Gray = im2[..., 2]
+    im1_gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
 
     # Detect ORB features and compute descriptors.
     orb = cv2.ORB_create(max_features)
-    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
-    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+    kp = orb.detect(im1_gray, None)
+    kp1, des1 = orb.compute(im1_gray, kp)
+    kp = orb.detect(im2_gray, None)
+    kp2, des2 = orb.compute(im2_gray, kp)
 
     # Match features.
     matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-    matches = matcher.match(descriptors1, descriptors2, None)
-
-    print("---")
+    matches = matcher.match(des1, des2, None)
 
     # Sort matches by score
     matches = list(matches)
     matches.sort(key=lambda x: x.distance, reverse=False)
 
-    print(len(matches))
     # Remove not so good matches
     numGoodMatches = int(len(matches) * good_match_percent)
     matches = matches[:numGoodMatches]
@@ -207,14 +205,14 @@ def alignImages(im1, im2, max_features=1000, good_match_percent=0.05):
     points2 = np.zeros((len(matches), 2), dtype=np.float32)
 
     for i, match in enumerate(matches):
-        points1[i, :] = keypoints1[match.queryIdx].pt
-        points2[i, :] = keypoints2[match.trainIdx].pt
+        points1[i, :] = kp1[match.queryIdx].pt
+        points2[i, :] = kp2[match.trainIdx].pt
 
     # Find homography
     h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
 
     # Use homography
     height, width, channels = im2.shape
-    im1Reg = cv2.warpPerspective(im1Gray, h, (width, height))
+    im1_reg = cv2.warpPerspective(im1, h, (width, height))
 
-    return im1Reg, h
+    return im1_reg, h
