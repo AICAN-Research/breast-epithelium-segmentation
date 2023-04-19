0ba1ecec5f0d4a3054908a598baa14cad198e19d
maren <maren.hoibo@ntnu.no>
2023-04-19 15:06:01 +0200

    fixed dtype

--- divide_data_wsi.py
+++ divide_data_wsi.py
@@ -11,7 +11,7 @@ import h5py
 
 curr_date = "".join(date.today().strftime("%d/%m").split("/")) + date.today().strftime("%Y")[2:]
 curr_time = "".join(str(datetime.now()).split(" ")[1].split(".")[0].split(":"))
-wsi_split_path = './wsi_splits/' + curr_date + "_" + curr_time + "/"
+wsi_split_path = './wsi_splits2/' + curr_date + "_" + curr_time + "/"
 
 N_train = 6
 N = np.arange(8)  # array from 0 (included) to 7 (included)
@@ -25,5 +25,5 @@ os.makedirs(wsi_split_path, exist_ok=True)
 
 # convert to hdf5
 with h5py.File((wsi_split_path + "dataset_split.h5"), 'w') as f:
-    f.create_dataset("val", data=np.array(val_set).astype('S400'), compression="gzip", compression_opts=4)
-    f.create_dataset("train", data=np.array(train_set).astype('S400'), compression="gzip", compression_opts=4)
+    f.create_dataset("val", data=np.array(val_set).astype('uint8'), compression="gzip", compression_opts=4)
+    f.create_dataset("train", data=np.array(train_set).astype('uint8'), compression="gzip", compression_opts=4)
