d9cb15a7ad0fcf688b6de8e0c623e224f19cc30f
maren <maren.hoibo@ntnu.no>
2023-04-28 07:59:50 +0200

    cleaned up

--- sandbox/eval_augment.py
+++ sandbox/eval_augment.py
@@ -10,7 +10,7 @@ import os
 from tqdm import tqdm
 from source.utils import normalize_img, patchReader
 from source.augment import random_rot90, random_fliplr, \
-    random_flipud, random_hue, random_saturation, random_brightness, random_blur
+    random_flipud, random_hue, random_saturation, random_brightness, random_blur, random_shift, random_contrast
 
 
 # evaluate augmentation
@@ -18,9 +18,9 @@ def eval_augmentation():
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     plot_flag = True
 
-    ds_name = '081222_135917_level_2_psize_512_ds_4/'  # change manually to determine desired dataset
+    ds_name = '200423_125554_level_2_psize_1024_ds_4/'  # change manually to determine desired dataset
 
-    ds_train_path = './datasets/' + ds_name + 'ds_train/invasive/'
+    ds_train_path = '/mnt/EncryptedSSD1/maren/datasets/' + ds_name + 'ds_train/invasive/'
 
     # load generated train dataset
     patches = os.listdir(ds_train_path)
@@ -34,44 +34,25 @@ def eval_augmentation():
     count = 0
     image_alls = []
     for image, mask in tqdm(ds_train):
-
-        image_all, mask_rot = random_rot90(image, mask)
-        image_all, mask_fup = random_flipud(image_all, mask)
-        image_all, mask_flr = random_fliplr(image_all, mask)
-        image_all = random_saturation(image_all, saturation=0.5)
-        image_all = random_hue(image_all, max_delta=0.05)
+        image_all, mask_flr = random_fliplr(image, mask)
+        image_all, mask_fup = random_flipud(image_all, mask_flr)
         image_all = random_brightness(image_all, brightness=0.2)
+        image_all = random_hue(image_all, max_delta=0.05)
+        image_all = random_saturation(image_all, saturation=0.5)
         image_all = random_blur(image_all)
         image_alls.append(image_all)
 
         if plot_flag and count > 8:
-            f, axes = plt.subplots(3, 3)  # Figure of the two corresponding TMAs
+            f, axes = plt.subplots(3, 3, figsize=(30, 30))
             axes[0, 0].imshow(image_alls[0])
-            axes[0, 0].set_title("all1")
-
             axes[0, 1].imshow(image_alls[1])
-            axes[0, 1].set_title("all2")
-
             axes[0, 2].imshow(image_alls[2])
-            axes[0, 2].set_title("all3")
-
             axes[1, 0].imshow(image_alls[3])
-            axes[1, 0].set_title("all4")
-
             axes[1, 1].imshow(image_alls[4])
-            axes[1, 1].set_title("all5")
-
             axes[1, 2].imshow(image_alls[5])
-            axes[1, 2].set_title("all6")
-
             axes[2, 0].imshow(image_alls[6])
-            axes[2, 0].set_title("all7")
-
             axes[2, 1].imshow(image_alls[7])
-            axes[2, 1].set_title("all8")
-
             axes[2, 2].imshow(image_alls[8])
-            axes[2, 2].set_title("all9")
 
             plt.show()
         count += 1
