f9a69df1eea29543efc211bddaa18afb9314db33
maren <maren.hoibo@ntnu.no>
2023-05-22 11:32:01 +0200

    added dice, precision, recall for all cylinders and those with class present

--- eval_tma_cylinders.py
+++ eval_tma_cylinders.py
@@ -188,7 +188,7 @@ class PadderPO(fast.PythonProcessObject):
         self.addOutputData(0, new_output_image)
 
 
-def eval_patch(path, model):
+def eval_patch(path, model, plot_flag):
 
     with h5py.File(path, "r") as f:
         image = np.asarray(f["input"])
@@ -216,46 +216,194 @@ def eval_patch(path, model):
     gt_shape = gt.shape
     pred = pred[:gt_shape[0], :gt_shape[1]]
 
-    print(pred.shape)
-    print(gt.shape)
+    #print(pred.shape)
+    #print(gt.shape)
 
     gt = np.argmax(gt, axis=-1).astype("uint8")
     pred = pred[..., 0].astype("uint8")
 
-    fig, ax = plt.subplots(1, 3, figsize=(30, 20))
-    ax[0].imshow(image)
-    ax[1].imshow(pred, vmin=0, vmax=3)
-    ax[2].imshow(gt, vmin=0, vmax=3)
-    plt.show()
+    if plot_flag:
+        fig, ax = plt.subplots(1, 3, figsize=(30, 20))
+        ax[0].imshow(image)
+        ax[1].imshow(pred, vmin=0, vmax=3)
+        ax[2].imshow(gt, vmin=0, vmax=3)
+        plt.show()
 
     print()
+
+    return image, gt, pred
 
 
 
 def eval_on_dataset():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     plot_flag = False
     bs = 1  # @TODO: should bs match bs in train?
 
     path = './datasets_tma_cores/150523_180206_level_1_ds_4/ds_val/'
-    model_name = './output/converted_models/model_120523_094220_agunet_bs_8_as_1_lr_0.0005_d__bl_1_br_0.2_h__s_0.2_st_1.0_mp_0_ntb_160_nvb_40.onnx'
+    model_name = './output/converted_models/model_140523_222332_agunet_bs_8_as_1_lr_0.0005_d__bl_1_br_0.2_h_0.05_s_0.2_st_1.0_mp_0_ntb_160_nvb_40.onnx'
 
     cylinders_paths = os.listdir(path)
     paths_ = np.array([path + x for x in cylinders_paths]).astype("U400")
 
+    dices_1 = []  # invasive
+    dices_2 = []  # benign
+    dices_3 = []  # insitu
+    dices_1_exist = []  # invasive
+    dices_2_exist = []  # benign
+    dices_3_exist = []  # insitu
+    precisions_1 = 0
+    precisions_2 = 0
+    precisions_3 = 0
+    precisions_1_exist = 0
+    precisions_2_exist = 0
+    precisions_3_exist = 0
+    recalls_1 = 0
+    recalls_2 = 0
+    recalls_3 = 0
+    recalls_1_exist = 0
+    recalls_2_exist = 0
+    recalls_3_exist = 0
+
+    cnt = 0  # number of cylinders in total
+    count_present_1_d = 0  # number of cylinders with invasive ep present
+    count_present_2_d = 0  # number of cylinders with benign ep present
+    count_present_3_d = 0  # number of cylinders with in situ ep present
+
+    count_present_1_p = 0  # number of cylinders with invasive ep present
+    count_present_2_p = 0  # number of cylinders with benign ep present
+    count_present_3_p = 0  # number of cylinders with in situ ep present
+
+    count_present_1_r = 0  # number of cylinders with invasive ep present
+    count_present_2_r = 0  # number of cylinders with benign ep present
+    count_present_3_r = 0  # number of cylinders with in situ ep present
+
+    cnt = 0
+
     for path in paths_:
-        eval_patch(path, model_name)
-
-
-    ds_val = tf.data.Dataset.from_tensor_slices(paths_)
-    ds_val = ds_val.map(lambda x: tf.py_function(eval_patch, [x], [tf.float32, tf.float32]), num_parallel_calls=8)
-    ds_val = ds_val.batch(bs)
-    ds_val.prefetch(1)
-
-    for image, mask in tqdm(ds_val):
+        image, gt, pred = eval_patch(path, model_name, plot_flag)
         print(image.shape)
-        print(mask.shape)
+        print(gt.shape)
+        print(pred.shape)
+        class_names = ["invasive", "benign", "insitu"]
+        for i, x in enumerate(class_names):
+            c_dice, union_d = class_dice_(gt, pred, class_val=i + 1)
+            c_precision = precision(gt, pred, object_=i + 1)
+            c_recall = recall(gt, pred, object_=i + 1)
+            if union_d:
+                c_dice = [c_dice]
+            else:
+                c_dice = [c_dice.numpy()]
 
+            c_dice_exist, count_d, union_d = class_dice_class_present(gt, pred, class_val=i + 1)
+            c_precision_exist, count_p = precision_class_present(gt, pred, object_=i + 1)
+            c_recall_exist, count_r = recall_class_present(gt, pred, object_=i + 1)
+            if union_d:
+                c_dice_exist = [c_dice_exist]
+            else:
+                c_dice_exist = [c_dice_exist.numpy()]
+
+            if i == 0:
+                dices_1.extend(c_dice)
+                precisions_1 += c_precision
+                recalls_1 += c_recall
+
+                if count_d:
+                    dices_1_exist.extend(c_dice_exist)
+                    count_present_1_d += 1
+                if count_p:
+                    precisions_1_exist += c_precision_exist
+                    count_present_1_p += 1
+                if count_r:
+                    recalls_1_exist += c_recall_exist
+                    count_present_1_r += 1
+
+            elif i == 1:
+                dices_2.extend(c_dice)
+                precisions_2 += c_precision
+                recalls_2 += c_recall
+
+                if count_d:
+                    dices_2_exist.extend(c_dice_exist)
+                    count_present_2_d += 1
+                if count_p:
+                    precisions_2_exist += c_precision_exist
+                    count_present_2_p += 1
+                if count_r:
+                    recalls_2_exist += c_recall_exist
+                    count_present_2_r += 1
+
+            elif i == 2:
+                dices_3.extend(c_dice)
+                precisions_3 += c_precision
+                recalls_3 += c_recall
+
+                if count_d:
+                    dices_3_exist.extend(c_dice_exist)
+                    count_present_3_d += 1
+                if count_p:
+                    precisions_3_exist += c_precision_exist
+                    count_present_3_p += 1
+                if count_r:
+                    recalls_3_exist += c_recall_exist
+                    count_present_3_r += 1
+
+        cnt = cnt + 1
+
+    print("cnt: ", cnt)
+    mu_1 = np.mean(dices_1)
+    mu_2 = np.mean(dices_2)
+    mu_3 = np.mean(dices_3)
+    p_1 = precisions_1 / cnt
+    p_2 = precisions_2 / cnt
+    p_3 = precisions_3 / cnt
+    r_1 = recalls_1 / cnt
+    r_2 = recalls_2 / cnt
+    r_3 = recalls_3 / cnt
+    # create 95%-CI
+    dice_ci_1, _ = BCa_interval_macro_metric(dices_1, func=lambda x: np.mean(x), B=10000)
+    dice_ci_2, _ = BCa_interval_macro_metric(dices_2, func=lambda x: np.mean(x), B=10000)
+    dice_ci_3, _ = BCa_interval_macro_metric(dices_3, func=lambda x: np.mean(x), B=10000)
+
+    mu_1_exist = np.mean(dices_1_exist)
+    mu_2_exist = np.mean(dices_2_exist)
+    mu_3_exist = np.mean(dices_3_exist)
+    p_1_exist = precisions_1_exist / count_present_1_p
+    p_2_exist = precisions_2_exist / count_present_2_p
+    p_3_exist = precisions_3_exist / count_present_3_p
+    r_1_exist = recalls_1_exist / count_present_1_r
+    r_2_exist = recalls_2_exist / count_present_2_r
+    r_3_exist = recalls_3_exist / count_present_3_r
+
+    print(mu_1, dice_ci_1)
+    print("mean precisions invasive: ", p_1)
+    print("mean recalls invasive: ", r_1)
+    print()
+    print(mu_2, dice_ci_2)
+    print("mean precisions benign: ", p_2)
+    print("mean recalls benign: ", r_2)
+    print()
+    print(mu_3, dice_ci_3)
+    print("mean precisions inSitu: ", p_3)
+    print("mean recalls inSitu: ", r_3)
+
+    print("EXISTS: ")
+    print(mu_1_exist)
+    print("mean precisions invasive exist: ", p_1_exist)
+    print("mean recalls invasive exist: ", r_1_exist)
+    print()
+    print(mu_2_exist)
+    print("mean precisions benign exist: ", p_2_exist)
+    print("mean recalls benign exist: ", r_2_exist)
+    print()
+    print(mu_3_exist)
+    print("mean precisions inSitu exist: ", p_3_exist)
+    print("mean recalls inSitu exist: ", r_3_exist)
+
+    print("COUNT:")
+    print(count_present_1_p, count_present_1_r, count_present_1_d)
+    print(count_present_2_p, count_present_2_r, count_present_2_d)
+    print(count_present_3_p, count_present_3_r, count_present_3_d)
 
 
 if __name__ == "__main__":
