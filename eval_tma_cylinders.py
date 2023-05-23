0f1a7cb45286c6b9fd6b587f4f6251f514f4e3ea
maren <maren.hoibo@ntnu.no>
2023-05-23 13:58:55 +0200

    multiprocessing

--- eval_tma_cylinders.py
+++ eval_tma_cylinders.py
@@ -1,25 +1,27 @@
 """
 Script for evaluating models trained on patche son 10x on whole TMA cylinders created at 20x
 """
+import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from stats import BCa_interval_macro_metric
 import h5py
+import multiprocessing as mp
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
-import fast
+#os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 
 # No smoothing when evaluating, to make differenciable during training
 def class_dice_(y_true, y_pred, class_val):
-    output1 = y_pred[:, :, class_val]
-    gt1 = y_true[:, :, class_val]
+    output1 = y_pred[..., class_val]
+    gt1 = y_true[..., class_val]
 
     intersection1 = tf.reduce_sum(output1 * gt1)
-    union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(gt1 * gt1)  #@TODO: why do we need output*output in reduce sum?
+    union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(
+        gt1 * gt1)  # @TODO: why do we need output*output in reduce sum?
     if union1 == 0:
         dice = 0.
         dice_u = True
@@ -33,11 +35,12 @@ def class_dice_(y_true, y_pred, class_val):
 def class_dice_class_present(y_true, y_pred, class_val):
     count = False
 
-    output1 = y_pred[:, :, class_val]
-    gt1 = y_true[:, :, class_val]
+    output1 = y_pred[..., class_val]
+    gt1 = y_true[..., class_val]
 
     intersection1 = tf.reduce_sum(output1 * gt1)
-    union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(gt1 * gt1)  #@TODO: why do we need output*output in reduce sum?
+    union1 = tf.reduce_sum(output1 * output1) + tf.reduce_sum(
+        gt1 * gt1)  # @TODO: why do we need output*output in reduce sum?
     if union1 == 0:
         dice = 0.
         dice_u = True
@@ -66,8 +69,8 @@ def precision(y_true, y_pred, object_):
     """
     precision_ = 0
 
-    output1 = y_pred[:, :, object_]
-    target1 = y_true[:, :, object_]
+    output1 = y_pred[..., object_]
+    target1 = y_true[..., object_]
 
     true_positives = tf.reduce_sum(target1 * output1)
     predicted_positives = tf.reduce_sum(output1)
@@ -92,8 +95,8 @@ def precision_class_present(y_true, y_pred, object_):
     precision_ = 0
     count = False
 
-    output1 = y_pred[:, :, object_]
-    target1 = y_true[:, :, object_]
+    output1 = y_pred[..., object_]
+    target1 = y_true[..., object_]
 
     true_positives = tf.reduce_sum(target1 * output1)
     predicted_positives = tf.reduce_sum(output1)
@@ -120,10 +123,11 @@ def recall(y_true, y_pred, object_):
     """
     recall_ = 0
 
-    output1 = y_pred[:, :, object_]
-    target1 = y_true[:, :, object_]
+    output1 = y_pred[..., object_]
+    target1 = y_true[..., object_]
 
-    true_positives = tf.reduce_sum(target1 * output1)  # TODO: consider reduce_sum vs K.sum, is there a difference in speed
+    true_positives = tf.reduce_sum(
+        target1 * output1)  # TODO: consider reduce_sum vs K.sum, is there a difference in speed
     possible_positives = tf.reduce_sum(target1)
     if possible_positives == 0:
         recall_ = 0
@@ -146,10 +150,11 @@ def recall_class_present(y_true, y_pred, object_):
     recall_ = 0
     count = False
 
-    output1 = y_pred[:, :, object_]
-    target1 = y_true[:, :, object_]
+    output1 = y_pred[..., object_]
+    target1 = y_true[..., object_]
 
-    true_positives = tf.reduce_sum(target1 * output1)  # TODO: consider reduce_sum vs K.sum, is there a difference in speed
+    true_positives = tf.reduce_sum(
+        target1 * output1)  # TODO: consider reduce_sum vs K.sum, is there a difference in speed
     possible_positives = tf.reduce_sum(target1)
     if possible_positives == 0:
         recall_ = 0
@@ -163,31 +168,34 @@ def recall_class_present(y_true, y_pred, object_):
 
 # @TODO: add Jaccard similarity coefficient score
 
-
-class PadderPO(fast.PythonProcessObject):
-    def __init__(self, width=1024, height=1024):
-        super().__init__()
-        self.createInputPort(0)
-        self.createOutputPort(0)
-
-        self.height = height
-        self.width = width
-
-    def execute(self):
-        # Get image and invert it with numpy
-        image = self.getInputData()
-        np_image = np.asarray(image)
-        tmp = np.zeros((self.height, self.width, 3), dtype="uint8")
-        shapes = np_image.shape
-        tmp[:shapes[0], :shapes[1]] = np_image
-
-        # Create new fast image and add as output
-        new_output_image = fast.Image.createFromArray(tmp)
-        new_output_image.setSpacing(image.getSpacing())
-        self.addOutputData(0, new_output_image)
+def eval_wrapper(some_inputs_):
+    return eval_patch(*some_inputs_)
 
 
-def eval_patch(path, model, plot_flag):
+def eval_patch(path, model):
+    import fast
+
+    class PadderPO(fast.PythonProcessObject):
+        def __init__(self, width=1024, height=1024):
+            super().__init__()
+            self.createInputPort(0)
+            self.createOutputPort(0)
+
+            self.height = height
+            self.width = width
+
+        def execute(self):
+            # Get image and invert it with numpy
+            image = self.getInputData()
+            np_image = np.asarray(image)
+            tmp = np.zeros((self.height, self.width, 3), dtype="uint8")
+            shapes = np_image.shape
+            tmp[:shapes[0], :shapes[1]] = np_image
+
+            # Create new fast image and add as output
+            new_output_image = fast.Image.createFromArray(tmp)
+            new_output_image.setSpacing(image.getSpacing())
+            self.addOutputData(0, new_output_image)
 
     with h5py.File(path, "r") as f:
         image = np.asarray(f["input"])
@@ -198,10 +206,10 @@ def eval_patch(path, model, plot_flag):
     data_fast = fast.Image.createFromArray(image)
     generator = fast.PatchGenerator.create(2048, 2048, overlapPercent=0.3).connect(0, data_fast)
     padder = PadderPO.create(width=2048, height=2048).connect(generator)
-    network = fast.NeuralNetwork.create(modelFilename=model, inferenceEngine="OpenVINO", scaleFactor=0.00392156862)\
+    network = fast.NeuralNetwork.create(modelFilename=model, inferenceEngine="OpenVINO", scaleFactor=0.00392156862) \
         .connect(padder)
     converter = fast.TensorToSegmentation.create(threshold=0.5).connect(0, network, 7)
-    resizer = fast.ImageResizer.create(width=2048, height=2048, useInterpolation=False, preserveAspectRatio=True)\
+    resizer = fast.ImageResizer.create(width=2048, height=2048, useInterpolation=False, preserveAspectRatio=True) \
         .connect(converter)
     stitcher = fast.PatchStitcher.create().connect(resizer)
 
@@ -212,23 +220,13 @@ def eval_patch(path, model, plot_flag):
 
     pred = np.asarray(pred)
 
+    del data_fast, generator, padder, network, converter, resizer, stitcher
+
     gt_shape = gt.shape
     pred = pred[:gt_shape[0], :gt_shape[1]]
 
-    #print(pred.shape)
-    #print(gt.shape)
-
     gt = np.argmax(gt, axis=-1).astype("uint8")
     pred = pred[..., 0].astype("uint8")
-
-    if plot_flag:
-        fig, ax = plt.subplots(1, 3, figsize=(30, 20))
-        ax[0].imshow(image)
-        ax[1].imshow(pred, vmin=0, vmax=3)
-        ax[2].imshow(gt, vmin=0, vmax=3)
-        plt.show()
-
-    print()
 
     # one-hot gt and pred
     gt_back = (gt == 0).astype("float32")
@@ -240,154 +238,126 @@ def eval_patch(path, model, plot_flag):
     pred_healthy = (pred == 2).astype("float32")
     pred_inSitu = (pred == 3).astype("float32")
 
-    gt_one_hot = np.stack(
+    gt = np.stack(
         [gt_back, gt_inv,
          gt_healthy, gt_inSitu], axis=-1)
-    pred_one_hot = np.stack(
+    pred = np.stack(
         [pred_back, pred_inv,
          pred_healthy, pred_inSitu], axis=-1)
 
-    return image, gt_one_hot, pred_one_hot
+    dice_scores = []
+    precisions_ = []
+    recalls_ = []
+    unions = []
 
+    dice_scores_exist = []
+    precisions_exists = []
+    recalls_exists = []
+    unions_exist = []
+    counts_d = []
+    counts_p = []
+    counts_r = []
+
+    class_names = ["invasive", "benign", "insitu"]
+    for i, x in enumerate(class_names):
+        c_dice, union_d = class_dice_(gt, pred, class_val=i + 1)
+        c_precision = precision(gt, pred, object_=i + 1)
+        c_recall = recall(gt, pred, object_=i + 1)
+        dice_scores.append(c_dice)  # list of three dices, one for each class
+        precisions_.append(c_precision)  # list of three precisions, one for each class
+        recalls_.append(c_recall)  # list of three recalls, one for each class
+        unions.append(union_d)  # list of three booleans, if true then union is zero and dice set to zero
+
+        c_dice_exist, count_d, union_d_exist = class_dice_class_present(gt, pred, class_val=i + 1)
+        c_precision_exist, count_p = precision_class_present(gt, pred, object_=i + 1)
+        c_recall_exist, count_r = recall_class_present(gt, pred, object_=i + 1)
+        dice_scores_exist.append(c_dice_exist)
+        precisions_exists.append(c_precision_exist)
+        recalls_exists.append(c_recall_exist)
+        unions_exist.append(union_d_exist)
+        counts_d.append(count_d)
+        counts_p.append(count_p)
+        counts_r.append(count_r)
+
+    return np.asarray(dice_scores), np.asarray(precisions_), np.asarray(recalls_), unions, \
+           np.asarray(dice_scores_exist), np.asarray(precisions_exists), np.asarray(recalls_exists), unions_exist, \
+           counts_d, counts_p, counts_r
 
 
 def eval_on_dataset():
-    #os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     plot_flag = False
-    bs = 1  # @TODO: should bs match bs in train?
 
     path = './datasets_tma_cores/150523_180206_level_1_ds_4/ds_val/'
     model_name = './output/converted_models/model_140523_222332_agunet_bs_8_as_1_lr_0.0005_d__bl_1_br_0.2_h_0.05_s_0.2_st_1.0_mp_0_ntb_160_nvb_40.onnx'
+    metrics_path = './eval_outputs/'
 
     cylinders_paths = os.listdir(path)
     paths_ = np.array([path + x for x in cylinders_paths]).astype("U400")
 
-    dices_1 = []  # invasive
-    dices_2 = []  # benign
-    dices_3 = []  # insitu
-    dices_1_exist = []  # invasive
-    dices_2_exist = []  # benign
-    dices_3_exist = []  # insitu
-    precisions_1 = 0
-    precisions_2 = 0
-    precisions_3 = 0
-    precisions_1_exist = 0
-    precisions_2_exist = 0
-    precisions_3_exist = 0
-    recalls_1 = 0
-    recalls_2 = 0
-    recalls_3 = 0
-    recalls_1_exist = 0
-    recalls_2_exist = 0
-    recalls_3_exist = 0
-
-    cnt = 0  # number of cylinders in total
-    count_present_1_d = 0  # number of cylinders with invasive ep present
-    count_present_2_d = 0  # number of cylinders with benign ep present
-    count_present_3_d = 0  # number of cylinders with in situ ep present
-
-    count_present_1_p = 0  # number of cylinders with invasive ep present
-    count_present_2_p = 0  # number of cylinders with benign ep present
-    count_present_3_p = 0  # number of cylinders with in situ ep present
-
-    count_present_1_r = 0  # number of cylinders with invasive ep present
-    count_present_2_r = 0  # number of cylinders with benign ep present
-    count_present_3_r = 0  # number of cylinders with in situ ep present
-
     cnt = 0
 
+    dice_scores_total = [[], [], []]
+    precisions_total = [[], [], []]
+    recalls_total = [[], [], []]
+    dice_scores_exist_total = [[], [], []]
+    precisions_exists_total = [[], [], []]
+    recalls_exists_total = [[], [], []]
     for path in paths_:
-        image, gt, pred = eval_patch(path, model_name, plot_flag)
+        inputs_ = [[path, model_name]]
+        p = mp.Pool(1)
+        output = p.map(eval_wrapper, inputs_)
+        output = output[0]
+
+        dice_scores, precisions_, recalls_, unions, dice_scores_exist, precisions_exists, recalls_exists, \
+        unions_exist, counts_d, counts_p, counts_r = output[0], output[1], output[2], output[3], output[4], output[5], \
+                                                     output[6], output[7], output[8], output[9], output[10]
+
+        p.terminate()
+        p.join()
+        del p, inputs_
+        cnt += 1
+
         class_names = ["invasive", "benign", "insitu"]
         for i, x in enumerate(class_names):
-            c_dice, union_d = class_dice_(gt, pred, class_val=i + 1)
-            c_precision = precision(gt, pred, object_=i + 1)
-            c_recall = recall(gt, pred, object_=i + 1)
-            if union_d:
-                c_dice = [c_dice]
-            else:
-                c_dice = [c_dice.numpy()]
+            dice_scores_total[i].append(dice_scores[i])
+            precisions_total[i].append(precisions_[i])
+            recalls_total[i].append(recalls_[i])
 
-            c_dice_exist, count_d, union_d = class_dice_class_present(gt, pred, class_val=i + 1)
-            c_precision_exist, count_p = precision_class_present(gt, pred, object_=i + 1)
-            c_recall_exist, count_r = recall_class_present(gt, pred, object_=i + 1)
-            if union_d:
-                c_dice_exist = [c_dice_exist]
-            else:
-                c_dice_exist = [c_dice_exist.numpy()]
+            if counts_d[i]:
+                dice_scores_exist_total[i].append(dice_scores_exist[i])
+            if counts_p[i]:
+                precisions_exists_total[i].append(precisions_exists[i])
+            if counts_r[i]:
+                recalls_exists_total[i].append(recalls_exists[i])
 
-            if i == 0:
-                dices_1.extend(c_dice)
-                precisions_1 += c_precision
-                recalls_1 += c_recall
-
-                if count_d:
-                    dices_1_exist.extend(c_dice_exist)
-                    count_present_1_d += 1
-                if count_p:
-                    precisions_1_exist += c_precision_exist
-                    count_present_1_p += 1
-                if count_r:
-                    recalls_1_exist += c_recall_exist
-                    count_present_1_r += 1
-
-            elif i == 1:
-                dices_2.extend(c_dice)
-                precisions_2 += c_precision
-                recalls_2 += c_recall
-
-                if count_d:
-                    dices_2_exist.extend(c_dice_exist)
-                    count_present_2_d += 1
-                if count_p:
-                    precisions_2_exist += c_precision_exist
-                    count_present_2_p += 1
-                if count_r:
-                    recalls_2_exist += c_recall_exist
-                    count_present_2_r += 1
-
-            elif i == 2:
-                dices_3.extend(c_dice)
-                precisions_3 += c_precision
-                recalls_3 += c_recall
-
-                if count_d:
-                    dices_3_exist.extend(c_dice_exist)
-                    count_present_3_d += 1
-                if count_p:
-                    precisions_3_exist += c_precision_exist
-                    count_present_3_p += 1
-                if count_r:
-                    recalls_3_exist += c_recall_exist
-                    count_present_3_r += 1
-
-        cnt = cnt + 1
-        print("count: ", cnt)
+        cnt += 1
 
     print("cnt: ", cnt)
-    mu_1 = np.mean(dices_1)
-    mu_2 = np.mean(dices_2)
-    mu_3 = np.mean(dices_3)
-    p_1 = precisions_1 / cnt
-    p_2 = precisions_2 / cnt
-    p_3 = precisions_3 / cnt
-    r_1 = recalls_1 / cnt
-    r_2 = recalls_2 / cnt
-    r_3 = recalls_3 / cnt
-    # create 95%-CI
-    dice_ci_1, _ = BCa_interval_macro_metric(dices_1, func=lambda x: np.mean(x), B=10000)
-    dice_ci_2, _ = BCa_interval_macro_metric(dices_2, func=lambda x: np.mean(x), B=10000)
-    dice_ci_3, _ = BCa_interval_macro_metric(dices_3, func=lambda x: np.mean(x), B=10000)
+    mu_1 = np.mean(dice_scores_total[0])
+    mu_2 = np.mean(dice_scores_total[1])
+    mu_3 = np.mean(dice_scores_total[2])
+    p_1 = np.mean(precisions_total[0])
+    p_2 = np.mean(precisions_total[1])
+    p_3 = np.mean(precisions_total[2])
+    r_1 = np.mean(recalls_total[0])
+    r_2 = np.mean(recalls_total[1])
+    r_3 = np.mean(recalls_total[2])
 
-    mu_1_exist = np.mean(dices_1_exist)
-    mu_2_exist = np.mean(dices_2_exist)
-    mu_3_exist = np.mean(dices_3_exist)
-    p_1_exist = precisions_1_exist / count_present_1_p
-    p_2_exist = precisions_2_exist / count_present_2_p
-    p_3_exist = precisions_3_exist / count_present_3_p
-    r_1_exist = recalls_1_exist / count_present_1_r
-    r_2_exist = recalls_2_exist / count_present_2_r
-    r_3_exist = recalls_3_exist / count_present_3_r
+    # create 95%-CI
+    dice_ci_1, _ = BCa_interval_macro_metric(dice_scores_total[0], func=lambda x: np.mean(x), B=10000)
+    dice_ci_2, _ = BCa_interval_macro_metric(dice_scores_total[1], func=lambda x: np.mean(x), B=10000)
+    dice_ci_3, _ = BCa_interval_macro_metric(dice_scores_total[2], func=lambda x: np.mean(x), B=10000)
+
+    mu_1_exist = np.mean(dice_scores_exist_total[0])
+    mu_2_exist = np.mean(dice_scores_exist_total[1])
+    mu_3_exist = np.mean(dice_scores_exist_total[2])
+    p_1_exist = np.mean(precisions_exists_total[0])
+    p_2_exist = np.mean(precisions_exists_total[1])
+    p_3_exist = np.mean(precisions_exists_total[2])
+    r_1_exist = np.mean(recalls_exists_total[0])
+    r_2_exist = np.mean(recalls_exists_total[1])
+    r_3_exist = np.mean(recalls_exists_total[2])
 
     print(mu_1, dice_ci_1)
     print("mean precisions invasive: ", p_1)
@@ -415,10 +385,11 @@ def eval_on_dataset():
     print("mean recalls inSitu exist: ", r_3_exist)
 
     print("COUNT:")
-    print(count_present_1_p, count_present_1_r, count_present_1_d)
-    print(count_present_2_p, count_present_2_r, count_present_2_d)
-    print(count_present_3_p, count_present_3_r, count_present_3_d)
+    print(len(dice_scores_exist_total[0]), len(precisions_exists_total[0]), len(recalls_exists_total[0]))
+    print(len(dice_scores_exist_total[1]), len(precisions_exists_total[1]), len(recalls_exists_total[1]))
+    print(len(dice_scores_exist_total[2]), len(precisions_exists_total[2]), len(recalls_exists_total[2]))
 
 
 if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     eval_on_dataset()
