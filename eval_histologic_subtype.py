3d88621d34659b23d3813a7ec71e3bce691f06c2
maren <maren.hoibo@ntnu.no>
2023-07-18 13:38:20 +0200

    skip cylinders without info on subtype and grade. added evaluation of grade.

--- eval_histologic_subtype.py
+++ eval_histologic_subtype.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
     # stata path, file which includes info on what subtype each case is
-    stata_path = '/data/Maren_P1/data/STATA/STATA_Maren_P1_edited.dta'
+    stata_path = '/data/Maren_P1/data/STATA/STATA_combined.dta'
 
     # dataset path
     dataset_path = './datasets_tma_cores/230623_110540_level_1_ds_4/ds_test/'
@@ -126,7 +126,9 @@ if __name__ == "__main__":
     # model path
     model_path = './output/converted_models/model_030623_224255_agunet_bs_8_as_1_lr_0.0005_d_None_bl_1_br_0.3_h_0.05_s_0.3_st_1.0_fl_1.0_rt_1.0_mp_0_ntb_160_nvb_40.onnx'
 
-    dice_types = [[[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []],[[], [], []]]
+    dice_types = [[[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []],
+                  [[], [], []]]
+    dice_grades = [[[], [], []], [[], [], []], [[], [], []]]
 
     for file in os.listdir(dataset_path):
         file_front = file.split(".")[0]
@@ -143,8 +145,13 @@ if __name__ == "__main__":
             & (df["case"] == int(case))
             ]
 
-        # get histological subtype for case
-        type_ = int(filtered_data["TYPE"])
+        # needs to skip cylinders without information on histological subtype/grade:
+        if len(filtered_data) == 0:
+            continue
+
+        # get histological subtype and grade for case
+        type_ = int(filtered_data["type_six"])  # 1, 2, 3, 4, 5, 8, still need eight in dice_types
+        grade_ = int(filtered_data["GRAD"])
 
         inputs_ = [[dataset_path + file, model_path]]
         p = mp.Pool(1)
@@ -157,16 +164,31 @@ if __name__ == "__main__":
         class_names = ["invasive", "benign", "insitu"]
         for i, x in enumerate(class_names):
             dice_types[type_ - 1][i].append(output[i])
+            dice_grades[grade_ - 1][i].append(output[i])
     
-    print("len 1: ", len(dice_types[0][0]), " mu 1 inv: ", np.mean(dice_types[0][0]), " mu 1 ben: ", np.mean(dice_types[0][1]), " mu 1 inv: ", np.mean(dice_types[0][2]))
-    print("len 2: ", len(dice_types[1][0]), " mu 2 inv: ", np.mean(dice_types[1][0]), " mu 1 ben: ", np.mean(dice_types[1][1]), " mu 1 inv: ", np.mean(dice_types[1][2]))
-    print("len 3: ", len(dice_types[2][0]), " mu 3 inv: ", np.mean(dice_types[2][0]), " mu 1 ben: ", np.mean(dice_types[2][1]), " mu 1 inv: ", np.mean(dice_types[2][2]))
-    print("len 4: ", len(dice_types[3][0]), " mu 4 inv: ", np.mean(dice_types[3][0]), " mu 1 ben: ", np.mean(dice_types[3][1]), " mu 1 inv: ", np.mean(dice_types[3][2]))
-    print("len 5: ", len(dice_types[4][0]), " mu 5 inv: ", np.mean(dice_types[4][0]), " mu 1 ben: ", np.mean(dice_types[4][1]), " mu 1 inv: ", np.mean(dice_types[4][2]))
-    print("len 6: ", len(dice_types[5][0]), " mu 6 inv: ", np.mean(dice_types[5][0]), " mu 1 ben: ", np.mean(dice_types[5][1]), " mu 1 inv: ", np.mean(dice_types[5][2]))
-    print("len 7: ", len(dice_types[6][0]), " mu 7 inv: ", np.mean(dice_types[6][0]), " mu 1 ben: ", np.mean(dice_types[6][1]), " mu 1 inv: ", np.mean(dice_types[6][2]))
-    print("len 8: ", len(dice_types[7][0]), " mu 8 inv: ", np.mean(dice_types[7][0]), " mu 1 ben: ", np.mean(dice_types[7][1]), " mu 1 inv: ", np.mean(dice_types[7][2]))
-
+    print("len 1: ", len(dice_types[0][0]), " mu 1 inv: ", np.mean(dice_types[0][0]), " mu 1 ben: ",
+          np.mean(dice_types[0][1]), " mu 1 ins: ", np.mean(dice_types[0][2]))
+    print("len 2: ", len(dice_types[1][0]), " mu 2 inv: ", np.mean(dice_types[1][0]), " mu 1 ben: ",
+          np.mean(dice_types[1][1]), " mu 1 ins: ", np.mean(dice_types[1][2]))
+    print("len 3: ", len(dice_types[2][0]), " mu 3 inv: ", np.mean(dice_types[2][0]), " mu 1 ben: ",
+          np.mean(dice_types[2][1]), " mu 1 ins: ", np.mean(dice_types[2][2]))
+    print("len 4: ", len(dice_types[3][0]), " mu 4 inv: ", np.mean(dice_types[3][0]), " mu 1 ben: ",
+          np.mean(dice_types[3][1]), " mu 1 ins: ", np.mean(dice_types[3][2]))
+    print("len 5: ", len(dice_types[4][0]), " mu 5 inv: ", np.mean(dice_types[4][0]), " mu 1 ben: ",
+          np.mean(dice_types[4][1]), " mu 1 ins: ", np.mean(dice_types[4][2]))
+    print("len 6: ", len(dice_types[5][0]), " mu 6 inv: ", np.mean(dice_types[5][0]), " mu 1 ben: ",
+          np.mean(dice_types[5][1]), " mu 1 ins: ", np.mean(dice_types[5][2]))
+    print("len 7: ", len(dice_types[6][0]), " mu 7 inv: ", np.mean(dice_types[6][0]), " mu 1 ben: ",
+          np.mean(dice_types[6][1]), " mu 1 ins: ", np.mean(dice_types[6][2]))
+    print("len 8: ", len(dice_types[7][0]), " mu 8 inv: ", np.mean(dice_types[7][0]), " mu 1 ben: ",
+          np.mean(dice_types[7][1]), " mu 1 ins: ", np.mean(dice_types[7][2]))
+    print()
+    print("len 1: ", len(dice_grades[0][0]), " mu 1 inv: ", np.mean(dice_grades[0][0]), " mu 1 ben: ",
+          np.mean(dice_grades[0][1]), " mu 1 ins: ", np.mean(dice_grades[0][2]))
+    print("len 2: ", len(dice_grades[1][0]), " mu 2 inv: ", np.mean(dice_grades[1][0]), " mu 1 ben: ",
+          np.mean(dice_grades[1][1]), " mu 1 ins: ", np.mean(dice_grades[1][2]))
+    print("len 3: ", len(dice_grades[2][0]), " mu 3 inv: ", np.mean(dice_grades[2][0]), " mu 1 ben: ",
+          np.mean(dice_grades[2][1]), " mu 1 ins: ", np.mean(dice_grades[2][2]))
 
 
 
