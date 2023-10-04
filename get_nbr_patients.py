6e701c12f387a2f37e17a38dab7af60c4ca34aee
maren <maren.hoibo@ntnu.no>
2023-10-04 14:55:32 +0200

    added possibility to check for each cohort

--- get_nbr_patients.py
+++ get_nbr_patients.py
@@ -1,12 +1,16 @@
 """
 Script for finding nbr of patients in a TMA-dataset (cylinders at image plane 1 or patches at image plane 2)
+Will not work when multiple triplets/dublets from same patient
+The files must be saved according to "create_data_tma.py" for patches from TMAs and to "create_tma_pairs.py" for whole
+TMA cylinders
 """
 import os
 
 level = 2  # level 1 = TMA-cylinder level, level 2 = patches
 
 if level == 1:
-    path = 'path to dataset'
+    ds_set = 1  # 1 = test set 1, 2 = test set 2
+    path = '/path/to/dataset'
 
     cylinders_paths = os.listdir(path)
 
@@ -14,10 +18,16 @@ if level == 1:
     checked_paths = []
 
     for path in cylinders_paths:
-        split = path.split('.')[0]
-        triplet_nbr = split.split('_')[5]
-        id_ = split.split('_')[4]
-        cohort = split.split('_')[3]
+        if ds_set == 1:
+            split = path.split('.')[0]
+            triplet_nbr = split.split('_')[5]
+            id_ = split.split('_')[4]
+            cohort = split.split('_')[3]
+        else:
+            split = path.split('.')[0]
+            triplet_nbr = split.split('_')[7]
+            id_ = split.split('_')[4]
+            cohort = split.split('_')[3]
 
         N = len(checked_paths)
         include = True
@@ -28,14 +38,13 @@ if level == 1:
             if cohort in checked_path and id_ == checked_path_id and triplet_nbr == checked_path_triplet:
                 include = False
                 continue
-        if include:
+        if include and cohort == 'HPA':  # if include and cohort == 'cohort name' if want to check for specific cohort
             checked_paths.append(path)
-
     print("nbr of patients in dataset is: ", len(checked_paths))
 
 # if patches:
 if level == 2:
-    path = 'path to dataset'
+    path = '/path/to/dataset'
 
     set = os.listdir(path)  # benign, in situ or invasive
 
@@ -66,7 +75,7 @@ if level == 2:
             if cohort in checked_path and id_ == checked_path_id and triplet_nbr == checked_path_triplet:
                 include = False
                 continue
-        if include:
+        if include and cohort == 'HUS':  # if include and cohort == 'cohort name' if want to check for specific cohort:
             checked_paths.append(path)
 
     print("nbr of patients in dataset is: ", len(checked_paths))
