292f01c33be3ca5523f20b7cc19f4d52dad431c7
maren <maren.hoibo@ntnu.no>
2023-11-16 23:43:13 +0100

    skip nan values by only looking and those greater than 0. Necessary when looking at only positive targets (when not evaluating all dice scores)

--- eval_std.py
+++ eval_std.py
@@ -3,14 +3,14 @@ Script for reading Dice scores from .csv files and calculating mean and std agai
 """
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv('/filepath/results.csv')
-print(df)
-df_filtered = df.iloc[0, :]  # find correct metric and class to evaluate
-print(df_filtered)
+path = '/path/to/file/.csv'
+df = pd.read_csv(path)
+df_filtered = df.iloc[11, :]  # find correct metric and class to evaluate (ex 11 which corresponds to ex in situ pos target)
 df_filtered = np.asarray(df_filtered)
 df_filtered = df_filtered[1:]  # remove column name from ndarray
-
-print(np.mean(df_filtered))
-print(np.std(df_filtered))
-print(np.std(df_filtered, ddof=1))
+df_new = []
+for elem in df_filtered:
+    if elem >= 0:
+        df_new.append(elem)
+df_new = np.asarray(df_new)
+print(np.std(df_new, ddof=1))
